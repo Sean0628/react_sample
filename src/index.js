5a6ff8aa34d6f148ed9dd333fcf0b54601fc8833
Sho Ito <i.sho.0628@gmail.com>
2019-02-09 09:45:01 +0900

    define onClick method

--- src/index.js
+++ src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
   render() {
     return (
       <button
       className="square"
-      onClick={() =>  this.setState({value: 'X'})}
+      onClick={() =>  this.props.onClick()}
       >
         {this.state.value}
       </button>
@@ -30,7 +24,12 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
